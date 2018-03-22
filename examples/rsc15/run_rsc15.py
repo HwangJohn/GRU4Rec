6f1d81bee500fdfe68c18ab17600e29d29dbd5f2
황중원 <angelic805@gmail.com>
2018-03-22 13:45:09 +0900

    change input path

--- examples/rsc15/run_rsc15.py
+++ examples/rsc15/run_rsc15.py
@@ -13,8 +13,8 @@ import pandas as pd
 import gru4rec
 import evaluation
 
-PATH_TO_TRAIN = '/path/to/rsc15_train_full.txt'
-PATH_TO_TEST = '/path/to/rsc15_test.txt'
+PATH_TO_TRAIN = '/Content/data/rsc15_train_full.txt'
+PATH_TO_TEST = '/content/data/rsc15_test.txt'
 
 if __name__ == '__main__':
     data = pd.read_csv(PATH_TO_TRAIN, sep='\t', dtype={'ItemId':np.int64})
